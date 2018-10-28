83981c10cfbe4e91b08772d8fe9e846fb3335cc8
UnknownMonk <smodcartoon@gmail.com>
2018-10-28 10:01:14 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+// CHALLENGE 1: REVERSE A STRING
+// Return a string in reverse
+// ex. reverseString('hello') === 'olleh'
+
+function reverseString(str) {
+
+  // const string = str.split('');
+  // string.reverse();
+  // string.join('');
+  // return string;
+
+  // let revString = '';
+  // for (let i = str.length - 1; i >= 0; i--) {
+  //   revString = revString + str[i];
+  // }
+  // return revString;
+
+  // let revString = '';
+  // for (let char of str) {
+  //   revString = str[i] + revString;
+  // }
+  // return revString;
+
+  let revString = '';
+  str.split('').forEach(char => revString = char +
+    revString);
+  return revString;
+
+
+}
+
+reverseString('hello');
+
+
+
+
+
+
+
+
+
+// CHALLENGE 2: VALIDATE A PALINDROME
+// Return true if palindrome and false if not
+// ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
+
+function isPalindrome(str) { }
+
+
+
+// CHALLENGE 3: REVERSE AN INTEGER
+// Return an integer in reverse
+// ex. reverseInt(521) === 125
+
+function reverseInt(int) { }
+
+
+
+// CHALLENGE 4: CAPITALIZE LETTERS
+// Return a string with the first letter of every word capitalized
+// ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
+function capitalizeLetters(str) { }
+
+
+
+// CHALLENGE 5: MAX CHARACTER
+// Return the character that is most common in a string
+// ex. maxCharacter('javascript') == 'a'
+function maxCharacter(str) { }
+
+
+
+// CHALLENGE 6: FIZZBUZZ
+// Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+function fizzBuzz() { }
