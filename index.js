2574857e89f53cc591042e94de07e087b9d0ecaa
UnknownMonk <smodcartoon@gmail.com>
2018-10-29 11:52:57 -0400

    third comit

--- index.js
+++ index.js
@@ -97,10 +97,46 @@ function capitalizeLetters(str) {
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) { }
+function maxCharacter(str) {
+
+  const charMap = {};
+  const maxNum = 0;
+  const maxChar = ''
+
+  str.split('').forEach(function (char) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  });
+
+  for (let char in charMap) {
+    if (charMap[char] > maxNum) {
+      maxNum = charMap[char];
+      maxChar = char;
+    }
+
+
+  }
+  return maxChar;
+}
 
 
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() { }
+function fizzBuzz() {
+
+  for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+      console.log('fizzbuzz')
+    } else if (i % 3 === 0) {
+      console.log('Fizz')
+    } else if (i % 5) {
+      console.log('Buzz');
+    } else {
+      console.log(i)
+    }
+  }
+}
