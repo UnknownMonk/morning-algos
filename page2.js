4424bd6af4b2330c489299c8adfffd2149863c7f
UnknownMonk <smodcartoon@gmail.com>
2018-11-02 09:40:58 -0400

    chunked arr

--- page2.js
+++ page2.js
@@ -34,9 +34,42 @@ function longestWord(sen) {
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
 
-function chunkArray(arr, len) { }
+function chunkArray(arr, len) {
+
+  // const chunkedArr = [];
+  // let i = 0;
+
+  // while (i < arr.length) {
+  //   chunkedArr.push(arr.slice(i, i + len));
+  //   i += len;
+  // }
+  // return chunkedArr;
+
+  const chunkedArr = []
+
+  arr.forEach(function (val) {
+    const last = chunkedArr[chunkedArr.length - 1];
+
+    if (!last || last.length === len) {
+      chunkedArr.push([val]);
+    } else {
+      last.push(val);
+    }
+
+
+
+  });
+
+  return chunkArr;
+}
+
+
+
+// console.log(chunkedArr);
+
 
 // CHALLENGE 3: FLATTEN ARRAY
+
 // Take an array of arrays and flatten to a single array
 // ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
 
@@ -57,6 +90,6 @@ function isAnagram(str1, str2) { }
 function letterChanges(str) { }
 
 // Call Function
-const output = longestWord('Hello, my name is Brad');
+const output = chunkArray([1, 2, 3, 4, 5, 6, 7], 2);
 
 console.log(output);
